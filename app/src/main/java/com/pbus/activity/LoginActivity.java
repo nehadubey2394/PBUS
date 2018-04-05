db9916aac3a9648342ddd4bffcbf804b204f57ad
Hemant <imhsharma1@gmail.com>
2018-04-05 19:39:22 +0530

    Initial commit

--- app/src/main/java/com/pbus/activity/LoginActivity.java
+++ app/src/main/java/com/pbus/activity/LoginActivity.java
@@ -1,15 +1,45 @@
 package com.pbus.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.pbus.R;
+import com.pbus.utility.Util;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Context context=this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setOnClick(findViewById(R.id.imgBack),findViewById(R.id.tvLogin));
+    }
+
+    private void setOnClick(View... view) {
+        for (View v:view)v.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgBack:
+                onBackPressed();
+                break;
+
+            case R.id.tvLogin:
+                Util.customToast(context,getResources().getString(R.string.underDev));
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
     }
 }
